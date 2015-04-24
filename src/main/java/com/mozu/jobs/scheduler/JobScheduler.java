8090de598d353be2b29eba2fa0854d878ef33806
Lakshmi Nair <lakshmi_nair@volusion.com>
2015-04-24 16:06:56 -0500

    Added a random number to minute and second parameter of cron scheduler. This is to fix PR defect 59488

--- src/main/java/com/mozu/jobs/scheduler/JobScheduler.java
+++ src/main/java/com/mozu/jobs/scheduler/JobScheduler.java
@@ -4,6 +4,7 @@
 
 package com.mozu.jobs.scheduler;
 
+import java.security.SecureRandom;
 import java.util.Calendar;
 import java.util.Properties;
 import java.util.TimeZone;
@@ -700,15 +701,28 @@ public class JobScheduler {
                 + ((siteId != null) ? "_" + siteId.toString() : "");
 
         TimeZone tz = TimeZone.getTimeZone(timezone);
-
+        
+        Integer[] randomNumbers=getRandomNumber();
+        minutes=String.valueOf(Integer.parseInt(minutes )+randomNumbers[0]);
+        String seconds=randomNumbers[1].toString();
+        
         // Create a cron trigger
         Trigger trigger = TriggerBuilder
                 .newTrigger()
                 .withIdentity(identity + TRIGGER, tenantIdString)
                 .withSchedule(
-                        CronScheduleBuilder.cronSchedule("0 " + minutes + " " + hours + " * * ?")
+                        CronScheduleBuilder.cronSchedule( seconds+ " " + minutes + " " + hours + " * * ?")
                                 .inTimeZone(tz)).usingJobData(TENANT_ID_KEY, tenantId)
                 .usingJobData(SITE_ID_KEY, siteId).usingJobData(JOB_NAME, identity).build();
         return trigger;
+    }
+    
+    private Integer[] getRandomNumber(){
+    	Integer[] randomNumbers=new Integer[2];
+    	SecureRandom sc= new SecureRandom();
+        int randomNumber=sc.nextInt(300);
+        randomNumbers[0]=randomNumber / 60;
+        randomNumbers[1]=randomNumber % 60;
+		return randomNumbers;
     }
 }
